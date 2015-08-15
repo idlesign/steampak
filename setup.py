d707b8d22ffef66b7c9d4e04f9d61b3db87537c8
idle sign <idlesign@yandex.ru>
2015-08-15 13:56:36 +0600

    setup.py updated.

--- setup.py
+++ setup.py
@@ -35,6 +35,9 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'License :: OSI Approved :: BSD License'
     ],
 )
