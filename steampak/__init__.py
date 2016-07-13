0be27562832c14ffe9b6d5f5bb9b733689f45cb3
idle sign <idlesign@yandex.ru>
2016-07-13 22:24:17 +0600

    Version number is bumped up.

--- steampak/__init__.py
+++ steampak/__init__.py
@@ -2,5 +2,5 @@ from .libsteam import Api as SteamApi, Application as SteamApplication, \
     Achievement as SteamAchievement, User as SteamUser, Dlc as SteamDlc
 
 
-VERSION = (0, 2, 0)
+VERSION = (0, 3, 0)
 
