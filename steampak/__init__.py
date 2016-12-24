e928ca0e52f324e6d3c2139818ad58341fec1fa9
idle sign <idlesign@yandex.ru>
2016-12-24 14:23:15 +0700

    Release v0.4.1

--- steampak/__init__.py
+++ steampak/__init__.py
@@ -2,4 +2,4 @@ from .libsteam import Api as SteamApi, Application as SteamApplication, \
     Achievement as SteamAchievement, User as SteamUser, Dlc as SteamDlc
 
 
-VERSION = (0, 4, 0)
+VERSION = (0, 4, 1)
