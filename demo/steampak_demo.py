b26634eafd16f0e84137231e742bce84c8c62181
idle sign <idlesign@yandex.ru>
2019-03-09 10:36:46 +0700

    Demo now uses 'arcade' package.

--- demo/steampak_demo.py
+++ demo/steampak_demo.py
@@ -3,7 +3,7 @@
 
 Before you start:
 
-* Demo application requires PyOpenGL (python3-opengl) and in some cases PyOpenGL-accelerate packages to be installed.
+* Demo application requires ``arcade`` packages to be installed: http://arcade.academy/installation_linux.html.
 * Make sure `steampak` is available to your Python 3.
 
 1. Set PATH_LIBSTEAM environment variable to a proper path or put `libsteam_api.so` into `demo` directory
@@ -19,70 +19,82 @@ Controls:
     * o - show overlay
     * p - show overlay with browser launched
 
+
+Steam debug hint:
+
+* Launch Steam with -console -debug_steamapi -dev
+* Open Steam -> Console, run ``log_ipc 1``, ``log_callbacks``
+* IPC log: tail-f ~/.steam/logs/ipc_SteamClient.log
+
 """
 from os import environ, path
 
-from OpenGL.GL import *
-from OpenGL.GLUT import *
-
-from steampak import SteamApi
-
-PATH_CURRENT = path.join(path.abspath(path.dirname(__file__)), 'libsteam_api.so')
+import arcade
 
 
-class SteampakDemo:
+class SteampakDemo(arcade.Window):
 
-    def __init__(self, fullscreen=False):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
 
-        glutInit()
-        glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
+        self._log = ''
 
-        if fullscreen:
-            glutGameModeString('1920x1080:32@60')
-            glutEnterGameMode()
+        try:
+            from steampak import SteamApi
+
+            steam = SteamApi(
+                environ.get('PATH_LIBSTEAM', path.join(path.abspath(path.dirname(__file__)), 'libsteam_api.so')),
+                app_id=environ.get('STEAM_APPID', 480),
+            )
+
+            steam_run_callbacks = steam.run_callbacks
+
+        except Exception as e:
+            self.log_add('exception: %s' % e)
+            steam = None
+            steam_run_callbacks = lambda: None
+
+        self.steam = steam
+        self.steam_run_callbacks = steam_run_callbacks
+
+    def on_draw(self):
+        arcade.start_render()
+        self.steam_run_callbacks()
+        arcade.draw_text(self._log, 10, 10, arcade.color.WHITE, anchor_y='bottom')
+
+    def log_add(self, val):
+        self._log += '\n%s' % val
+
+    def on_key_press(self, key, key_modifiers):
+        log_add = self.log_add
+
+        log_add('key: %s' % key)
+
+        if key == 113:
+            log_add('closing')
+            self.close()
 
         else:
-            glutInitWindowPosition(100, 100)
-            glutInitWindowSize(1024, 768)
-            glutCreateWindow('steampak demo')
 
-        glutDisplayFunc(self.redraw)
-        glutIdleFunc(self.redraw)
-        glutKeyboardFunc(self.keypress)
+            try:
 
-        api = SteamApi(
-            environ.get('PATH_LIBSTEAM', PATH_CURRENT),
-            app_id=environ.get('STEAM_APPID', 480),
-        )
-        self.api = api
+                if key == 111:
+                    log_add('overlay activate')
+                    self.steam.overlay.activate()
 
-    def redraw(self):
-        self.api.run_callbacks()
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glutSwapBuffers()
+                elif key == 115:
+                    log_add('take screenshot')
+                    self.steam.screenshots.take()
 
-    def keypress(self, key, x, y):
+                elif key == 112:
+                    log_add('overlay link')
+                    self.steam.overlay.activate('https://github.com/idlesign/steampak/')
 
-        print('Key pressed: %s' % key)
-
-        if key == b'q':
-            self.api.shutdown()
-            sys.exit()
-
-        elif key == b'o':
-            self.api.overlay.activate()
-
-        elif key == b's':
-            self.api.screenshots.take()
-
-        elif key == b'p':
-            self.api.overlay.activate('https://github.com/idlesign/steampak/')
-
-    def run(self):
-        glutMainLoop()
+            except Exception as e:
+                log_add('exception: %s' % e)
 
 
 if __name__ == '__main__':
 
-    demo = SteampakDemo()
-    demo.run()
+    SteampakDemo(800, 600, 'steampak demo')
+    arcade.run()
